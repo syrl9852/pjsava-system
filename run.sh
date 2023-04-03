dfeb8d9c892201be118a16d49712f38ac67e4588
saikyounosennsi <sato18782@gmail.com>
2023-04-03 23:18:41 +0900

    run.shに排他処理を追加

--- run.sh
+++ run.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-echo "===============`date "+%Y/%m/%d %H:%M:%S"`開始===============" >> run.log
-npx node ./index.js  2>&1 | while read -r line; do echo "$line" | xargs -d "\n" printf "`date "+%Y/%m/%d %H:%M:%S"` : %s\n" &>> run.log; done
-echo "===============`date "+%Y/%m/%d %H:%M:%S"`終了exitコード`echo ${PIPESTATUS[0]}`===============" >> run.log
+
+if [[ $1 = '0001' ]]; then
+	echo "===============`date "+%Y/%m/%d %H:%M:%S"`開始===============" >> run.log
+	npx node ./index.js  2>&1 | while read -r line; do echo "$line" | xargs -d "\n" printf "`date "+%Y/%m/%d %H:%M:%S"` : %s\n" &>> run.log; done
+	echo "===============`date "+%Y/%m/%d %H:%M:%S"`終了exitコード`echo ${PIPESTATUS[0]}`===============" >> run.log
+else
+	cd $(dirname ${0})
+	flock --timeout=0 ./run.log ${0} 0001
+fi
