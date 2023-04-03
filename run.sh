72e0191705671b55d9611d99f27afb41828d55d6
syota_sacv01 <soutagol1@gmail.com>
2023-04-04 00:27:51 +0900

    開始コマンドの修正

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 if [[ $1 = '0001' ]]; then
 	echo "===============`date "+%Y/%m/%d %H:%M:%S"`開始===============" >> run.log
-	npx node ./index.js  2>&1 | while read -r line; do echo "$line" | xargs -d "\n" printf "`date "+%Y/%m/%d %H:%M:%S"` : %s\n" &>> run.log; done
+	npm start  2>&1 | while read -r line; do echo "$line" | xargs -d "\n" printf "`date "+%Y/%m/%d %H:%M:%S"` : %s\n" &>> run.log; done
 	echo "===============`date "+%Y/%m/%d %H:%M:%S"`終了exitコード`echo ${PIPESTATUS[0]}`===============" >> run.log
 else
 	cd $(dirname ${0})
